5f5f4d437a0ac7ff1a302cfb1beff46a8ae3f7a2
Michael Quint <mjq8@cornell.edu>
2017-09-25 12:54:26 -0700

    fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ user2 = User.create!({username: "DIYDoug",
    password: "123456",
    img_url: "https://res.cloudinary.com/make-anything/image/upload/v1506023305/aih7neya2wkohnstfyei.png"})
 user3 = User.create!({username: "ProjectPaula",
-   email: "PaulaPoops@drama.com",
+   email: "PaulaPlease@drama.com",
     password: "123456",
     img_url: "https://res.cloudinary.com/make-anything/image/upload/v1505956289/tadjexic3gzxircnins4.png"})
 
@@ -75,28 +75,28 @@ project10 = Project.create!({title: "How to Make a Killer DIY Project Instructio
 step1 = Step.create!({title: "Plan and Acquire Tools",
    img_url: "https://res.cloudinary.com/make-anything/image/upload/c_scale,w_782/v1506099757/Chisel-Hammer-Work-Repair-Tools-Screwdriver-2145770_wuwrod.jpg",
    description: "Collect a hammer, two screwdrivers, 3 nails, and lots of wood.  Also make sure to create a detailed diagram with measurements to ensure success of the project.  Never go into something without a solid plan!",
-   project_id: 1})
+   project_id: project1.id})
 
 step2 = Step.create!({title: "Build a Frame",
    description: "This is no simple feat!  The frame is the base of the entire project so make sure to pay attention to detail.  Use your tools and follow your plans, I believe in you.",
-   project_id: 1})
+   project_id: project1.id})
 
 step3 = Step.create!({title: "Paint and Add Components",
    img_url: "https://res.cloudinary.com/make-anything/image/upload/c_scale,w_594/v1506100180/6293727982_8411741334_b_ltvtob.jpg",
    description: "This part is very important.  Paint it whatever color your hard desires but make sure it really splashes and dazzles the eyes.  If a skeeball game is ugly nobody will want to play it, guarenteed.  Also at this point you should install the ramp, the cups to catch the points with the associated score labelled on each cup. If safety is of concern or you have aggressive children maybe you should install a net to catch flying balls from damaging your wall and flooring.",
-   project_id: 1})
+   project_id: project1.id})
 
 step4 = Step.create!({title: "Step to test out styling",
    video_url: "https://www.youtube.com/watch?v=HC7JK5EP0qE",
    description: "This part is also very important.  I am wondering what it will look if a step has just a video and no pictures.",
-   project_id: 1})
+   project_id: project1.id})
 
 step5 = Step.create!({title: "Another Step to test out styling",
    img_url: "https://res.cloudinary.com/make-anything/image/upload/c_scale,w_594/v1506100180/6293727982_8411741334_b_ltvtob.jpg",
    description: "This part is important too!  I want to make sure everything looks good when I have picture and video in the same step",
    video_url: "https://www.youtube.com/watch?v=1q6bZlHOCTA",
-   project_id: 1})
+   project_id: project1.id})
 
 step6 = Step.create!({title: "Congratulate yourself!",
    description: "That was a long journey, but you achieved excellent.  Take a deep breath, pat yourself on the back and enjoy your game.  Happy Skeeballin' :).",
-   project_id: 1})
+   project_id: project1.id})
