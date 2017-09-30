9eb6eb66b53a5df248163402dcf9ca32a2c83dd3
Michael Quint <mjq8@cornell.edu>
2017-09-29 23:37:18 -0700

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
 var path = require("path");
 
-var webpack = require("webpack");
-
-var plugins = []; // if using any plugins for both dev and production
-var devPlugins = []; // if using any plugins for development
-
-var prodPlugins = [
+// var webpack = require("webpack");
+//
+// var plugins = []; // if using any plugins for both dev and production
+// var devPlugins = []; // if using any plugins for development
+//
+// var prodPlugins = [
   // new webpack.DefinePlugin({
   //   'process.env': {
   //     'NODE_ENV': JSON.stringify('production')
   //   }
   // }),
-  new webpack.DefinePlugin({
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
-  }),
-  new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: true
-    }
-  })
-];
-
-plugins = plugins.concat(
-  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
-);
+//   new webpack.DefinePlugin({
+//     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+//   }),
+//   new webpack.optimize.UglifyJsPlugin({
+//     compress: {
+//       warnings: true
+//     }
+//   })
+// ];
+//
+// plugins = plugins.concat(
+//   process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+// );
 
 module.exports = {
   context: __dirname,
@@ -32,7 +32,7 @@ module.exports = {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: "bundle.js"
   },
-  plugins: plugins,
+  // plugins: plugins,
   module: {
     loaders: [
       {
