4e66b51f9461cfc5765703a85775c725ab062482
Michael Quint <mjq8@cornell.edu>
2017-09-29 22:27:33 -0700

    fix?

--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,13 @@ var plugins = []; // if using any plugins for both dev and production
 var devPlugins = []; // if using any plugins for development
 
 var prodPlugins = [
+  // new webpack.DefinePlugin({
+  //   'process.env': {
+  //     'NODE_ENV': JSON.stringify('production')
+  //   }
+  // }),
   new webpack.DefinePlugin({
-    'process.env': {
-      'NODE_ENV': JSON.stringify('production')
-    }
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
   }),
   new webpack.optimize.UglifyJsPlugin({
     compress: {
