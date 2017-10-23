896ea6f1fc240e4003e9a88a878f03d138db4fe0
Michael Quint <mjq8@cornell.edu>
2017-10-23 16:32:16 -0700

    fix minification warnings

--- webpack.config.js
+++ webpack.config.js
@@ -11,9 +11,9 @@ var prodPlugins = [
       'NODE_ENV': JSON.stringify('production')
     }
   }),
-  new webpack.DefinePlugin({
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
-  }),
+  // new webpack.DefinePlugin({
+  //   'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+  // }),
   new webpack.optimize.UglifyJsPlugin({
     compress: {
       warnings: true
