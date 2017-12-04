4ed591177e306b660879ac91917e70f3e994c706
Michael Quint <mjq8@cornell.edu>
2017-12-04 14:02:48 -0800

    updated resume

--- frontend/components/resume/resume.jsx
+++ frontend/components/resume/resume.jsx
@@ -4,7 +4,7 @@ class Resume extends React.Component {
   render() {
     return (
       <div>
-        <embed className="resume" src="https://res.cloudinary.com/make-anything/image/upload/v1512353652/Michael_Quint_Resume.pdf" width="850px" height="1100px" />
+        <embed className="resume" src="https://res.cloudinary.com/make-anything/image/upload/v1512424817/Michael_Quint_Resume.pdf" width="850px" height="1100px" />
       </div>
     );
   }
